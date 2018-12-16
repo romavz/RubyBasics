53d47a9e8205ce2d1dfed0d4f61746432c36742e
Roman. Z <zworkbox@yandex.ru>
2018-12-16 04:08:46 +0400

    Часть8. Ответ 1.1

--- Part8/passenger_wagon.rb
+++ Part8/passenger_wagon.rb
@@ -4,13 +4,15 @@ class PassengerWagon < Wagon
   INVALID_CAPACITY_MESSAGE = 'Количество мест должно быть в пределах 1..100, задано: '.freeze
   ALL_SPACE_TAKEN_MESSAGE = 'Все места уже заняты'.freeze
   NOT_ENOUGH_SPACE_MESSAGE = 'Количесва свободных мест недостаточно'.freeze
+  SEATS_COUNT_TOO_MUTCH = "Количесвто занимаемых мест за раз должно быть 1".freeze
 
   def properties
     @properties[:capacity] = "количество мест: #{capacity}"
     super
   end
 
-  def take_a_space
+  def take_a_space(seat_count)
+    rise ArgumentError, SEATS_COUNT_TOO_MUTCH if seat_count != 1
     super(1)
   end
 
